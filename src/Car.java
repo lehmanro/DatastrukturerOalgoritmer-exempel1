2ccc1b3e301e000c436016f95f4849a38b83dfd8
lehmanro <lehmanro@arcada.fi>
2024-01-08 22:26:54 +0200

    Lektion1 exempel getters and setters

---
+++ src/Car.java
@@ -0,0 +1,16 @@
+public class Car {
+
+    private String name; // Vi deklarerar instansvariabeln name
+
+    public Car(String name) {
+        this.name = name;
+    }
+    // Getter
+    public String getName() {
+        return name;
+    }
+    // Setter
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello again !");
+
+        // Instansiering av objekt
+        Car car = new Car("Tesla");
+        Car anotherCar = new Car("Fiat");
+
+        // Skriv ut namnet via getter-metoden
+        System.out.println(car.getName());
+
+        // Getter och Setter
+        System.out.println(anotherCar.getName());
+        anotherCar.setName("Fiat 500");
+        System.out.println(anotherCar.getName());
+
+
     }
 }
