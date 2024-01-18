85b8a4e2013f9964e1c8121721f1d76acdc26f63
lehmanro <lehmanro@arcada.fi>
2024-01-18 11:54:16 +0200

    CC final commit

--- src/Main.java
+++ src/Main.java
@@ -6,167 +6,40 @@ public class Main {
     static final double GAME_VERSION = 1.0;
     public static void main(String[] args) {
 
-
-        Random random = new Random();
-
+    Random random = new Random();
 
         Scanner intInput = new Scanner(System.in);
         Scanner strInput = new Scanner(System.in);
 
-        System.out.println(random.nextInt(99));
-        System.out.println(random.nextInt(100,999));
-
-        //System.exit(0);
-
-        Calculator calc = new Calculator();
-        System.out.println(calc.multiply(3, 5));
-        System.out.println(calc.multiply(3, 5, 6));
-
-        //System.out.printf("Skriv ditt namn: ");
-        //String userName = strInput.nextLine();
-        //System.out.printf("Hej %s!\n", userName);
 
 
 
+        //String stuff = strInput.nextLine();
 
         while (true) {
-
             try {
-                System.out.printf("Skriv ett tal att multiplicera: ");
-                int userIntInput = Integer.parseInt(strInput.nextLine());
-                /*System.out.printf("Skriv ett tal till: ");
-                int userIntInput2 = intInput.nextInt();*/
-                System.out.printf("Svar: 5 * %d = %d\n",
-                        userIntInput,
-                       /* userIntInput2,*/
-                        calc.multiply(/*userIntInput2*/5, userIntInput)
-                );
-                break;
-            }
-            catch (NumberFormatException e){
-                System.out.println("Du ska ge ett heltal");
-            }
-            catch (Exception e) {
-                System.out.println("Någonting gick fel");
-            }
+                System.out.println("Skriv Enter för att fortsätta q för att avsluta");
 
+                System.out.println("Slumptal: " + random.nextInt(1,100));
+                //String stuff = strInput.nextLine();
+
+
+
+                if (strInput.nextLine().equals("q")) {
+                    //String stuff2 = strInput.nextLine();
+                    System.out.println("Hejdå! ");
+                    System.exit(0);
+
+                }
+
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Skriv tal");
+
+            }
         }
 
 
 
-
-
-
-
-        /*System.out.println();
-
-
-        // Instansiering av objekt
-
-
-        //Funkar ej med abstract: Clothing hat = new Clothing();
-
-        Scanner intInput = new Scanner(System.in)
-
-
-
-        // default-konstruktor
-        Foo foo = new Foo();
-
-        Bike bike = new Bike("Nishiki", 500);
-
-        BikeShop bikeShop = new BikeShop();
-        bikeShop.addBike(bike);
-        bikeShop.addBike(new Bike("Kona", 600));
-        bikeShop.addBike(new Bike("Jopo", 400));
-
-        for (int i = 0; i < bikeShop.getBikes().size(); i++) {
-
-            System.out.printf("%d %s %.2f\n",
-                    i,
-                    bikeShop.getBikes().get(i).getName(),
-                    bikeShop.getBikes().get(i).getPrice());
-
-
-        }
-
-
-
-
-        Shirt shirt = new Shirt("Gant");
-        shirt.setPrice(100);
-        shirt.getCondition();
-        System.out.println(shirt.getCondition());
-        System.out.println(shirt.getBrand() + " Skjortan kostar " + shirt.getPrice());
-
-        Shirt shirt2 = new Shirt("Dressman", 30);
-        System.out.println(shirt2.getBrand() + " Skjortan kostar " + shirt2.getPrice());
-
-        Shirt shirt3 = new Shirt();
-        System.out.println(shirt3.getBrand() + " Skjortan kostar " + shirt3.getPrice());
-
-
-        Car car = new Car("Tesla");
-        System.out.println(String.format("Brand: %s, it is  %s and it cocts %.2f",
-                car.getName(),
-                car.getType(),
-                car.getPrice()));
-
-
-        Car car2 = new Car("Fiat");
-
-        HashMap<String,Car> drivers = new HashMap<>();
-        drivers.put("Kalle", car2);
-        drivers.put("Lisa", car);
-
-        // put skriver också updatering.
-        drivers.put("Lisa", new Car("Toyota"));
-
-        System.out.printf("Kalle drives a %s and \nLisa drives a %s \n",
-                drivers.get("Kalle").getName(),
-                drivers.get("Lisa").getName()
-                );
-
-        System.out.printf("Brand: %s, it is  %s and it costs %.2f\n",
-                car2.getName(),
-                car2.getType(),
-                car2.getPrice());
-
-        car.refuel();
-
-
-
-
-
-        Vehicle todaysRide = new Car("VW");
-
-        System.out.println(todaysRide.getName());
-        todaysRide = new Bike("Jopo");
-        System.out.println(todaysRide.getName());
-
-        // Obs objekt namn är referensser till samma objekt
-        Vehicle tomorrowsRide = todaysRide; // Gotcha! Inte en kopia!
-        tomorrowsRide.setName("tomorrow");
-        System.out.println(tomorrowsRide.getName());
-        System.out.println(todaysRide.getName());
-        // Samma objektreferens!
-
-        todaysRide.getPrice();
-
-
-        // Skriv ut namnet via getter-metoden
-        System.out.println(car.getName() + " (" + car.getType() + ")");
-        car.soundWarning();
-        System.out.println(car.getName() + " Kostar:" + car.price + "€ (FEL!)");
-        System.out.println(car.getName() + " Kostar:" + car.getPrice() + "€ (Rätt!)");
-
-        System.out.println(bike.getName() + " (" + bike.getType() + ")");
-        bike.soundWarning();
-
-        System.out.println("total cars created: " + car.getcarsCreated());
-
-
-
-*/
     }
 }
