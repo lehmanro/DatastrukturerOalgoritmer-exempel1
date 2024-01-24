f400f9c4009fb5253296a4afa7eb95e76c1c7e82
lehmanro <lehmanro@arcada.fi>
2024-01-24 15:59:41 +0200

    CC commit

--- src/Main.java
+++ src/Main.java
@@ -4,44 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner strInput = new Scanner(System.in);
+        //Scanner strInput = new Scanner(System.in);
 
-        BikeShop shop = new BikeShop();
-        shop.addBike(new Bike("Scott", 200));
-        shop.addBike(Bike.createRandomBike("Nishiki"));
-        shop.addBike(new Bike("Jopo", Utils.getRandomPrice()));
+        int myInt = Utils.getUserInput();
 
-
-        System.out.println("Välj cykel att köpa:");
-        for (int i = 0; i < shop.getBikes().size(); i++) {
-            Bike bike = shop.getBikes().get(i);
-            System.out.printf("[%d] %s kostar %.2f\n",
-                    i,
-                    bike.getName(),
-                    bike.getPrice());
-        }
-        int chosenBike;
-
-        while (true) {
-            System.out.println("Vilken cykel vill du köpa");
-
-
-            String userInput = strInput.nextLine();
-
-            if (Utils.isInteger(userInput)) {
-                chosenBike = Integer.parseInt(userInput);
-                break;
-            }
-            System.out.println("Du måste ge ett tal");
-        }
-        System.out.printf("Grattis, du äger en %S", shop.getBikes().get(chosenBike).getName());
-
-
-
-
-
-
-        //CCRandom.generate();
+        System.out.printf("Du returnerar nu ett heltal %d"
+                ,myInt
+                );
 
 
     }
--- src/Utils.java
+++ src/Utils.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Utils {
 
@@ -8,6 +9,30 @@ public class Utils {
     static double getRandomPrice(){
         Random rnd = new Random();
         return rnd.nextDouble(100, 1000);
+    }
+    static int getUserInput(){
+
+        Scanner strInput = new Scanner(System.in);
+        while (true) {
+            try {
+
+                System.out.printf("Skriv in ett tal\n");
+                String userInput = strInput.nextLine();
+                return Integer.parseInt(userInput);
+
+            }
+            catch (Exception e) {
+                System.out.println("Du måste ge ett tal");
+
+            }
+
+
+        }
+
+
+
+
+
     }
 
 }
