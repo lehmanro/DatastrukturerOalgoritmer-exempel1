861de451f415bd0d05c344747941ba1a95e62ae1
”lehmanro” <lehmanro@arcada.fi>
2024-02-08 15:23:54 +0200

    cc commit

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 
+        String[] testCharacters = {"a","b","c","d","e","f"};
+        StringArray test = new StringArray(testCharacters);
+
+        System.out.println("test size: " + test.size());
+        //System.out.println(test.toString());
+        System.out.println(test);
+        test.add("g");
+        System.out.println(test);
+        test.add(5, "h");
+        System.out.println(test);
+        test.reverse();
+        System.out.println(test);
+
 
         new BigO();
 
